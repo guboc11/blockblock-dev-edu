589c4caebd4e2f30cc3cc8918b95266b2f801e56
Taewon Park <guboc11@gmail.com>
2024-12-28 11:46:22 +0000

    3주차 블록블록 홈페이지

---
+++ 3-week/React/taewon/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+        
+      <div>
+        <input placeholder="약속">sdsdf</input>
+      </div>
+      
+    </div>
+  )
+}
+
+export default App;
+
