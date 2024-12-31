e16946f26a2f99e9e7baefd2f2fe91481461fc35
guboc11 <guboc11@gmail.com>
2024-12-31 16:48:11 +0900

    Update blockblock-prototype : add routing feature

--- 3-week/React/blockblock-prototype/src/App.js
+++ 3-week/React/blockblock-prototype/src/App.js
@@ -1,9 +1,31 @@
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+// import About from "./pages/About";
+// import Contact from "./pages/Contact";
+
+const App = () => {
   return (
-    <div className="App">
-      <div className="text-4xl">blockblock</div>
-    </div>
+    <Router>
+      <nav className="p-4 bg-gray-100">
+        <div className="flex space-x-8">
+          <Link to="/" className="text-2xl text-blue-500 hover:underline">
+            Home
+          </Link>
+          <Link to="/" className="text-2xl text-blue-500 hover:underline">
+            Home
+          </Link>
+        </div>
+      </nav>
+      <div className="p-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} /> */}
+        </Routes>
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
---
+++ 3-week/React/blockblock-prototype/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Welcome to the Home Page</h1>
+      <p className="mt-2">This is the main page of the application.</p>
+    </div>
+  );
+};
+
+export default Home;
